7549079f246dd9d90e0dc59b401a270fcdae6263
camachocris <camachocris49@gmail.com>
2022-04-12 11:35:10 -0500

    Upload

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,11 +1,13 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::system_program;
+//use anchor_lang::solana_program::system_program;
+
 declare_id!("F4MpghVxRA1hu6aot5NAwdmLFK2zQnArNwPYnuKMStep");
 
 #[program]
 pub mod solana_twitter {
     use super::*;
     pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+    //pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
@@ -32,8 +34,9 @@ pub struct SendTweet<'info> {
     pub tweet: Account<'info, Tweet>,
     #[account(mut)]
     pub author: Signer<'info>,
-    #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>,
+    //#[account(address = system_program::ID)]
+    //pub system_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
@@ -59,7 +62,8 @@ impl Tweet {
         + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; 
 }
 
-#[error_code]
+#[error]
+//#[error_code]
 pub enum ErrorCode {
     #[msg("The provided topic should be 50 characters long maximum.")]
     TopicTooLong,
--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -21,6 +21,49 @@ describe('solana-twitter', () => {
       signers: [tweet],
     });
     const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
-  	console.log(tweetAccount);
+  	
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'veganism');
+    assert.equal(tweetAccount.content, 'Hummus, am i rigth?');
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it('can send a new tweet without a topic', async() => {
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('', 'gm', {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
+    });
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  	
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, '');
+    assert.equal(tweetAccount.content, 'gm');
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it('can send a new tweet from a different author', async() => {
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('veganism', 'cebolla junca!', {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [otherUser, tweet],
+    });
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  	
+    assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'veganism');
+    assert.equal(tweetAccount.content, 'cebolla junca!');
+    assert.ok(tweetAccount.timestamp);
   });
 });
