ccd6d4a89d1d04a39780c7b1ecb67b9cd92cde82
camachocris <camachocris49@gmail.com>
2022-04-11 23:33:18 -0500

    first commit

---
+++ tests/solana-twitter.ts
@@ -0,0 +1,26 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { SolanaTwitter } from '../target/types/solana_twitter';
+import * as assert from "assert";
+
+describe('solana-twitter', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
+
+  it('can send a new tweet', async() => {
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('veganism', 'Hummus, am i rigth?', {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
+    });
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  	console.log(tweetAccount);
+  });
+});
